b7685334e32dca577f3d871d82ca43828d9348c9
FabriceZangl <fabrice@zangl.eu>
2015-08-19 00:57:46 +0200

    Signed-off-by: FabriceZangl <fabrice@zangl.eu>

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,17 @@
 ## Otherwise, the second function will calcualted the inversion.
 ## This allows to improve performance of the overall code.
 
-## Below function creates a special "matrix" object that can cache its inverse.
+## Below function creates an object that can cache & read from cache a matrix and its inverse.
 
-makeCacheMatrix <- function(x = matrix()) {          ##
-     minv <- NULL                                    ##
-     setinverse <- function(solve) minv <<- solve    ##
-     getinverse <- function() minv                   ##
-     list(setinverse = setinverse,                   ##
+makeCacheMatrix <- function(x = matrix()) {
+     mat <- x
+     set <- function(y) {
+          x <<- y                                    ## caches the matrix (to matrix of higher environment)
+     }
+     get <- function() x                             ## Reads matrix from cache.
+     setinverse <- function(z) minv <<- solve        ## Caches the inverse of the matrix.
+     getinverse <- function() minv                   ## Reads the inverse of the matrix.
+     list(setinverse = setinverse,                   ## Output is the list of functions.
           getinverse = getinverse)        
 }
 
@@ -17,16 +21,19 @@ makeCacheMatrix <- function(x = matrix()) {          ##
 ## Below function calculates the inverse of the special "matrix" returned by makeCacheMatrix above.
 ## If it has already been calculated (and the matrix has not changed), 
 ## then the cachesolve should retrieve the inverse from the cache.
+## If the matrix has changed, it will also recalculate the inverse.
 
 cacheSolve <- function(x, ...) {
-     minv <- x$getinverse()                ## minv is variable that will have the inverted matrix as output. This line gets the inverted matrix, whether it's empty or not.
-     if(!is.null(minv)) {                  ## checking whether there is an inversion (minv) already calculated for matrix x. 
+     minv <- x$getinverse()                ## Reads inverted matrix from cache.
+     chg <- TRUE                           ## Verifies that matrix hasn't changed.
+     if(!is.null(minv) && chg) {           ## Checks whether an inverse is stored in cache and whether the matrix in cache is still the same.
           message("getting cached data")
           return(minv)                     ## If so, the function will simply return minv as the inverted matrix.
      }
      else{
-          minv <- solve(x, ...)            ## This line actually calculates the inversion of matrix x.
-          x$setinverse(minv)               ## This line calls the makeCacheMatrix to store/cache the calculated inversion in a seperate environment.
+          mat <- x$get()                   ## Otherwise, read matrix from cache...
+          minv <- solve(mat, ...)          ## and calculate its inverse.
+          x$setinverse(minv)               ## Then, cache the calculated inverted matrix.
           return(minv)                     ## The result of the inversion calculated just 2 lines above is returned by this function.
      }
 }
